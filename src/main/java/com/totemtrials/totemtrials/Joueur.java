8d5e389fdfd774fb5dd46db98b9f50e624b8ebaa
Evan <chennevierevan@gmail.com>
2026-03-18 12:21:14 +0100

    Animation des jetons

--- src/main/java/com/totemtrials/totemtrials/Joueur.java
+++ src/main/java/com/totemtrials/totemtrials/Joueur.java
@@ -1,4 +1,14 @@
 package com.totemtrials.totemtrials;
 
 public class Joueur {
+
+    private String nom;
+    private Jeton jetonJoueur;
+
+    public Joueur(String nom, Jeton jetonJoueur) {
+        this.nom = nom;
+        this.jetonJoueur = jetonJoueur;
+    }
+
+
 }
