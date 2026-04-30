c77c78b6b1de7e6cd1079097f12cf9d4f192e729
EvanChennevier <chennevierevan@gmail.com>
2026-04-30 13:05:07 +0200

    Ajout de test

--- src/test/java/com/totemtrials/totemtrials/models/UnitTest.java
+++ src/test/java/com/totemtrials/totemtrials/models/UnitTest.java
@@ -1,5 +1,6 @@
 package com.totemtrials.totemtrials.models;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,7 +17,7 @@ class UnitTest {
 
         assertThrows(IllegalArgumentException.class, () -> {
             p.initJoueurs(1);
-        }, "La partie ne devrait pas accepter 1 seul joueur.");
+        }, "The game can't accept 1 player");
     }
 
     //Test when you try to create with 2 players
@@ -36,8 +37,45 @@ class UnitTest {
 
         assertThrows(IllegalArgumentException.class, () -> {
             p.initJoueurs(5);
-        }, "La partie ne devrait pas accepter plus de 4 joueurs.");
+        }, "The game can't accept more 4 players");
     }
 
-    //
+    //Assign token test
+    @Test
+    void SameTokenNameTest() {
+        Partie p = new Partie();
+        p.initJoueurs(2);
+        List<Joueur> joueurs = List.of(p.getJoueurs());
+        Joueur p1 = joueurs.get(0);
+        Joueur p2 = joueurs.get(1);
+
+        Jeton tiger1 = new Jeton("Tiger", "He can advance 1 more if he won a versus.", null, null);
+        Jeton tiger2 = new Jeton("Tiger", "He can dance", null, null);
+
+        p1.setJeton(tiger1);
+        p2.setJeton(tiger2);
+
+        assertEquals(p1.getJeton().getNom(), p2.getJeton().getNom(), "Sames names.");
+    }
+
+    //Assign token test
+    @Test
+    void SameTokenPassiveTest() {
+        Partie p = new Partie();
+        p.initJoueurs(2);
+        List<Joueur> joueurs = List.of(p.getJoueurs());
+        Joueur p1 = joueurs.get(0);
+        Joueur p2 = joueurs.get(1);
+
+        Jeton tiger1 = new Jeton("Tiger", "He can advance 1 more if he won a versus.", null, null);
+        Jeton tiger2 = new Jeton("Snake", "He can advance 1 more if he won a versus.", null, null);
+
+        p1.setJeton(tiger1);
+        p2.setJeton(tiger2);
+
+        assertEquals(p1.getJeton().getPassif(), p2.getJeton().getPassif(), "Sames passives.");
+    }
+
+
+
 }
